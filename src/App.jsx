ab5cb0c790f03ab1a5fbf930d90f8d7324f2256a
DeepakMehra53 <dmehra594@gmail.com>
2025-03-24 23:29:16 +0530

    Error when remove react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import { Home } from "./components/Home";
-
+import { SecondPage } from "./components/SecondPage";
+import { useState } from "react";
 export const App = () => {
+  const [page, setPage] = useState("home");
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      {page === "home" && <Home setPage={setPage} />}
+      {page === "second" && <SecondPage />}
     </>
   );
 };
+
