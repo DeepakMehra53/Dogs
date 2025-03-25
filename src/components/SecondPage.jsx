652b4bcf35a8c4afd355ba0e46d73a5098a1c4a0
DeepakMehra53 <dmehra594@gmail.com>
2025-03-25 10:58:23 +0530

    Creating Product card page

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,7 +1,10 @@
+import SecondPageimg from '../assets/SecondPage.png'
 export const SecondPage = () =>{
     return (
-        <div className="">
-            
+      <div className="">
+        <div className="flex justify-center mt-10 ">
+          <img src={SecondPageimg} alt="" className="w-[60rem] " />
         </div>
-    )
+      </div>
+    );
 }
